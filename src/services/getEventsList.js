d538e3f5d00dbcfe5d541dc9466c200f2f21458b
snehareddy <snehareddy0806@gmail.com>
2021-02-10 10:12:40 -0600

    fix: avoid mixed content

--- src/services/getEventsList.js
+++ src/services/getEventsList.js
@@ -1,4 +1,4 @@
 export function getEventList() {
-    return fetch('http://www.mocky.io/v2/59f08692310000b4130e9f71')
+    return fetch('https://www.mocky.io/v2/59f08692310000b4130e9f71')
         .then(data => data.json())
 }
